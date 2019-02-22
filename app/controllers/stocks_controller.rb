49973665aa5a1cbb210bf44107b86b63c776ae7d
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-02-22 18:11:04 +0530

    Handle invalid search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,19 @@
 class StocksController < ApplicationController
 
 	def search
-		@stock = Stock.new_from_lookup(params[:stock])
-		# render json: @stock
-		render 'users/my_portfolio' 
+		if params[:stock].present?
+			@stock = Stock.new_from_lookup(params[:stock])
+			# render json: @stock
+			if @stock
+				render 'users/my_portfolio' 
+			else
+				flash[:danger] = "You have entered an incorrect symbol."
+				redirect_to my_portfolio_path
+			end
+		else
+			flash[:danger] = "You have entered an empty string."
+			redirect_to my_portfolio_path
+		end
 	end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,14 @@
 class Stock < ApplicationRecord
 
 	def self.new_from_lookup(ticker_symbol)
-		looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-		# price = strip_commas(looked_up_stock.latest_price)
-		price = looked_up_stock.latest_price
-		new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
+		begin
+			looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+			# price = strip_commas(looked_up_stock.latest_price)
+			price = looked_up_stock.latest_price
+			new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
+		rescue Exception => e
+			return nil
+		end
 	end
 
 	def self.strip_commas(number)
