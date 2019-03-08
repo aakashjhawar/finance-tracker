e7a3f6a10299217ccd38cbc087f43d19eb7822a2
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-08 16:48:59 +0530

    Friendship views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	end
 
 	def my_friends
-
+		@friendships = current_user.friends
 	end
 		
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my-portfolio', to: 'users#my_portfolio'
-  get 'my-friends', to: 'users#my_friends'
   get 'search-stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'my-friends', to: 'users#my_friends'
+  
 end
