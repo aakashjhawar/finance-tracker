73261aacb52e318e66284d8bca404488e0499fa4
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-11 01:26:28 +0530

    Search actions and errors for my-friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,15 @@ class UsersController < ApplicationController
 		
 	def search
 		# puts params[:search_param]
-		@user = User.search(params[:search_param])
-		render json: @user
-	end
+		if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users match this search criteria" if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
 
 end
