94f75b156632177f516cb44c9dbb231ad6db7375
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-12 01:42:53 +0530

    Friends show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,4 +34,9 @@ class UsersController < ApplicationController
     redirect_to my_friends_path
   end
   
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
+  end
+  
 end
